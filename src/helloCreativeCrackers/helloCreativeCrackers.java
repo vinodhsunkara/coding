0293cb11980b69b6f82aa11a39990305f817b6d3
Vinodh Kumar Sunkara <vinsun@yahoo-inc.com>
2015-09-16 22:42:59 -0700

    Hello World

---
+++ src/helloCreativeCrackers/helloCreativeCrackers.java
@@ -0,0 +1,10 @@
+package helloCreativeCrackers;
+
+/**
+ * Created by vinsun on 9/16/15.
+ */
+public class helloCreativeCrackers {
+	public static void main(String[] args) {
+		System.out.println("Creative Crackers!");
+	}
+}
